394e34d420e3bc1c00f16d55ac1f854d8b3ec0a8
zhangjie <3zhangjie@libii.com>
2025-06-10 18:16:42 +0800

    优化代码

--- JToolKits.cpp
+++ JToolKits.cpp
@@ -4,19 +4,36 @@
 #include "hook_manager.h"
 #include <iostream>
 
+void* old_messagebox = nullptr;
+void* old_messagebox_handler = nullptr;
 int WINAPI MyMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType)
 {
     // 你可以在这里自定义行为
     return 0; // 或调用原始 MessageBoxW
 }
 
+void HookAPIs(HookManager& hook_manager)
+{
+    hook_manager.Begin();
+    HMODULE dll = GetModuleHandleA("user32.dll");
+    old_messagebox_handler = hook_manager.HookAPI(dll, "MessageBoxW", (void*)MyMessageBoxW, true, &old_messagebox);
+    hook_manager.End();
+}
+
+void UnhookAPIs(HookManager& hook_manager)
+{
+    hook_manager.Begin();
+    hook_manager.UnhookAPI(old_messagebox_handler);
+    hook_manager.End();
+}
+
 int main()
 {
     HMODULE hUser32 = LoadLibraryA("user32.dll");
-    void* oldMessageBoxW = nullptr;
+
     HookManager mgr;
-    auto old_handler = mgr.HookAPI(hUser32, "MessageBoxW", (void*)MyMessageBoxW, true, &oldMessageBoxW);
+    HookAPIs(mgr);
     MessageBox(NULL, L"Hello, World!", L"Hooked MessageBox<0>", MB_OK);
-    mgr.UnhookAPI(old_handler);
+    UnhookAPIs(mgr);
     MessageBox(NULL, L"Hello, World!", L"Hooked MessageBox<1>", MB_OK);
 }
